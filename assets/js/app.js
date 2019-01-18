1a21932c97c29d74df00f092a9ff3d04ce01badd
Thierry STOLLSTEINER <stollsteiner.thierry@hotmail.fr>
2019-01-18 10:56:02 +0100

    mise en place du Home controller et de la route d'entrée

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,10 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+import "bootstrap";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
